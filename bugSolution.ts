6f2b23997bf44f688cecae9ff02d83c682398373
BugHunter-X <askzenai@gmail.com>
2024-12-30 08:22:04 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,13 @@
+function printCoord(pt: { x?: number; y?: number }) {
+  if (pt.x !== undefined) {
+    console.log("The coordinate's x value is " + pt.x);
+  }
+  if (pt.y !== undefined) {
+    console.log("The coordinate's y value is " + pt.y);
+  }
+}
+
+printCoord({ x: 3, y: 7 });
+printCoord({ x: 3 });
+printCoord({ y: 7 });
+printCoord({});
